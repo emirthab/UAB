0efc912e845311efc08eff9a26c266bdd1a4db89
Emir <52050284+emirthab@users.noreply.github.com>
2021-10-02 17:57:10 +0300

    engeller

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -3,8 +3,3 @@ extends Node
 var gameStatus = true
 var coin = 0
 
-func _ready():
-	playSceneAnim()
-
-func playSceneAnim():
-	get_tree().current_scene.get_node("AnimationPlayer").play("anim")
--- scripts/people.gd
+++ scripts/people.gd
@@ -2,15 +2,15 @@ extends RigidBody
 
 func _ready():
 	$Area.connect("body_entered",self,"_on_Body_entered")
-	$model/AnimationPlayer.get_animation("courier|mixamocom|Layer0").loop = true
+	var animName = $model/AnimationPlayer.get_animation_list()[0]
+	$model/AnimationPlayer.get_animation(str(animName)).loop = true
 	$model/AnimationPlayer.playback_speed = 2
-	$model/AnimationPlayer.play("courier|mixamocom|Layer0")
-	$model/AnimationPlayer.play("courier|mixamocom|Layer0")
-#	$model/AnimationPlayer.get_animation("courier|mixamocom|Layer0").loop = true
+	$model/AnimationPlayer.play(str(animName))
 
 
 func _on_Body_entered(body):
 	if body.name == "player":
+		add_force(Vector3(0,0,0),Vector3(0,0.2,0.3))
 		Globals.gameStatus = false
 		var uiLose = preload("res://assets/gui/lose.tscn").instance()
 		get_tree().current_scene.get_node("AnimationPlayer").stop()
