9a4c04881c307ad41d41082248d46ce68d3ddf3b
Emir <52050284+emirthab@users.noreply.github.com>
2021-10-02 18:02:20 +0300

    Revert "Merge branch 'main' of https://github.com/emirthab/UAB into main"
    This reverts commit 05fd226b400c1d07d7b30b92784ade630ba5d004, reversing
    changes made to 93ba90371cda4361552930ab6ebd2e81f6c2c8b1.

--- scripts/playerMovement.gd
+++ scripts/playerMovement.gd
@@ -1,21 +1,21 @@
 extends VehicleBody
 
-var velocityMax = 1000
-var accelSpeed = 1
+var velocityMax = 30
+var accelSpeed = 60
 
 var status = 0
 
 func _physics_process(delta):
 	if Globals.gameStatus:
 		if Input.is_action_pressed("move"):
-			set_engine_force(-400)
+			linear_velocity.z -= accelSpeed * delta
 			if status == 1:
 				status = 0
 				$astronaut/AnimationPlayer.play_backwards("metarigAction")
 
 		else:
-			set_engine_force(350)
+			linear_velocity.z += accelSpeed * delta
 			if status == 0:
 				status = 1
 				$astronaut/AnimationPlayer.play("metarigAction")
-
+		linear_velocity.z = clamp(linear_velocity.z,0,velocityMax)
