e1e11aee874f5b45ab89da9e351ac4152c404cc4
Emir <52050284+emirthab@users.noreply.github.com>
2021-10-03 18:03:03 +0300

    metrobus

--- scripts/busMovement.gd
+++ scripts/busMovement.gd
@@ -4,12 +4,11 @@ var velocityMax = 1000
 var accelSpeed = 1
 
 func _physics_process(delta):
-	if Globals.gameStatus:
-		if Input.is_action_pressed("move"):
-			if linear_velocity.x > 0:
-				set_engine_force(-300)
-			else:
-				set_engine_force(0)
-
+	if Input.is_action_pressed("move"):
+		if linear_velocity.x > 0:
+			set_engine_force(-300)
 		else:
-			set_engine_force(250)
+			set_engine_force(0)
+
+	else:
+		set_engine_force(250)
