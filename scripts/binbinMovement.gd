99e7ec88509cdf270684aec826edcc29051ead2f
Emir <52050284+emirthab@users.noreply.github.com>
2021-10-03 11:08:08 +0300

    stop mechanic and bus bug fix

--- scripts/binbinMovement.gd
+++ scripts/binbinMovement.gd
@@ -8,7 +8,10 @@ var status = 0
 func _physics_process(delta):
 	if Globals.gameStatus:
 		if Input.is_action_pressed("move"):
-			set_engine_force(-400)
+			if linear_velocity.z > 0:
+				set_engine_force(-400)
+			else:
+				set_engine_force(0)
 			if status == 1:
 				status = 0
 				$astronaut/AnimationPlayer.play_backwards("metarigAction")
