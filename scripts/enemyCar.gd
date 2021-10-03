2444f4741138983a79020b03376ab4dd63b8f6c3
Emir <52050284+emirthab@users.noreply.github.com>
2021-10-03 12:46:36 +0300

    giprobe

--- scripts/enemyCar.gd
+++ scripts/enemyCar.gd
@@ -7,5 +7,5 @@ func entered(body):
 	if body.name == "player":
 		Globals.gameStatus = false
 		var uiLose = preload("res://assets/gui/lose.tscn").instance()
-		get_tree().current_scene.get_node("AnimationPlayer").stop()
+		get_node("./AnimationPlayer").stop()
 		get_tree().current_scene.add_child(uiLose)
