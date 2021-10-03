fc8142002969b74785bdc648284afab0dc668d7c
Emir <52050284+emirthab@users.noreply.github.com>
2021-10-03 19:57:42 +0300

    space

---
+++ scripts/rocket.gd
@@ -0,0 +1,14 @@
+extends RigidBody
+
+var velocityMax = 1000
+var accelSpeed = 1
+
+func _physics_process(delta):
+	print(linear_velocity)
+	if Input.is_action_pressed("move"):
+		if linear_velocity.x > 0:
+			add_central_force(Vector3(-10,0,0))
+
+	else:
+		if linear_velocity.x < 200:
+			add_central_force(Vector3(10,0,0))
