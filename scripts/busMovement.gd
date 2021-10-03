e1115aba19b1e5b3e9a83deba11da7d213ad6897
Emir <52050284+emirthab@users.noreply.github.com>
2021-10-03 18:45:29 +0300

    bus speed. up

--- scripts/busMovement.gd
+++ scripts/busMovement.gd
@@ -6,9 +6,11 @@ var accelSpeed = 1
 func _physics_process(delta):
 	if Input.is_action_pressed("move"):
 		if linear_velocity.x > 0:
-			set_engine_force(-300)
+			set_engine_force(-500)
 		else:
 			set_engine_force(0)
 
 	else:
-		set_engine_force(250)
+		if linear_velocity.x < 300:
+			add_central_force(Vector3(1,0,0))
+			set_engine_force(750)
